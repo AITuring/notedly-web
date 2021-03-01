8b9a4f9a281c34e865db9468c8125d712a5e1bdc
AITuring <shspku365@gmail.com>
2021-03-01 17:50:39 +0800

    增加了react页面导航

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
+import Header from '../components/Header'
+import Navigation from '../components/Navigation'
+
 const Home = () => {
     return (
         <div>
+            <Header />
+            <Navigation />
             <h1>Notedly</h1>
             <p>This is the home page</p>
         </div>
