27a33353772bf520bf7cb14f3108e9534f45969a
AITuring <shspku365@gmail.com>
2021-03-02 18:14:33 +0800

    增加部分全局样式,今天写算法,没写notedly,效率好低

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,16 @@
 import React from 'react'
 
+import styled from 'styled-components';
+
 import Header from './Header'
 import Navigation from './Navigation'
+
+const Wrapper = styled.div`
+    @media (min-width: 700px) {
+        display: flex;
+        top:
+    }
+`
 
 const Layout = ({ children }) => {
     return (
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import Button from '../components/Button'
 
 const Home = () => {
     return (
         <div>
             <p>This is the home page</p>
+            <Button>Click me!</Button>
         </div>
     )
 }
