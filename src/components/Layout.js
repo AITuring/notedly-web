7f6ced4f7c5638232de3359f34cf9acd0e3fba0d
AITuring <shspku365@gmail.com>
2021-03-04 21:27:05 +0800

    增加了一点点main样式,tm还不下班,太恶心了

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,8 +8,19 @@ import Navigation from './Navigation'
 const Wrapper = styled.div`
     @media (min-width: 700px) {
         display: flex;
-        top:
+        top:64px;
+        position:relative;
+        height: calc(100% -64px);
+        width: 100%;
+        flex: auto;
+        flex-direction: column;
     }
+`
+
+const Main = styled.main`
+    position: fixed;
+    height: calc(100% -185px);
+    
 `
 
 const Layout = ({ children }) => {
