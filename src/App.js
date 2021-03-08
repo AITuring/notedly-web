6face3332699b5c7690073d8109a6a1d0a96c94a
AITuring <shspku365@gmail.com>
2021-03-09 00:08:47 +0800

    后端出问题了,前端无法继续了

--- src/App.js
+++ src/App.js
@@ -2,17 +2,30 @@
 // This is the main entry point of our application
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+// 导入apollo clent库
+import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client'
+// 全局样式
 import GlobalStyle from '/components/GlobalStyle'
-
+// 路由
 import Pages from './pages'
+
+// 配置API URI和缓存
+const uri = process.env.API_URI
+const cache = new InMemoryCache()
+
+// 配置Apollo client
+const client = new ApolloClient({
+    uri,
+    cache,
+    connectToDevTools: true
+})
 
 const App = () => {
     return (
-        <div>
+        <ApolloProvider client={client}>
             <GlobalStyle />
             <Pages />
-        </div>
+        </ApolloProvider>
     )
 }
 
